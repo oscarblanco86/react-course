b1e07db935f0f94f3bd5ce32613009df69a15dc5
Oscar Blanco <oscarmb86@hotmail.com>
2024-11-12 23:36:56 -0600

    first commit, created the base template of a react course

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,114 @@
+import { NavLink } from "react-router-dom"
+
+const Navbar = () => {
+    // let activeStyle = {
+    //     textDecoration: "underline"
+    // }
+    const activeStyle = 'underline underline-offset-4'
+    return(
+        <nav className="flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light">
+            <ul className="flex items-center gap-3">
+                <li className="font-semibold text-lg">
+                    <NavLink 
+                        to='/'
+                        className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>
+                        Shopi
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                    to='/all'
+                    className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>
+                        all
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                    to='/clothes'
+                    className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>
+                        Clothes    
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                    to='/electronics'
+                    className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>
+                        Electronics    
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                    to='/furnitures'
+                    className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>
+                        Furnitures    
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                    to='/toys'
+                    className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>
+                        Toys    
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                    to='/others'
+                    className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>
+                        Others    
+                    </NavLink>
+                </li>
+            </ul>
+            <ul className="flex items-center gap-3">
+                <li className="text-black/60">
+                    oscar@mail.com
+                </li>
+                <li>
+                    <NavLink 
+                    to='/my-orders'
+                    className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>
+                        My Orders
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                    to='/my-account'
+                    className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>
+                        My Account    
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                    to='/sign-in'
+                    className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>
+                        Sign In    
+                    </NavLink>
+                </li>
+                <li>
+                    🛒
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
